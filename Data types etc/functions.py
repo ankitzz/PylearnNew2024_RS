d29a721392cd5868fea0fbaeced775361f7165ae
AnkitSharma <ankit.cargof@gmail.com>
2024-06-03 08:46:44 +0530

    test git commit

---
+++ Data types etc/functions.py
@@ -0,0 +1,20 @@
+# In python, function is a group of related statements that perform a specific task.
+
+def GreetMe():
+    print('gm')
+#Function call
+GreetMe()
+
+
+def Country(name):
+    print('India ' + name) #name is varible & India is string
+Country("Ankit")
+
+def AddIntegers(a,b):
+    print(a+b)
+AddIntegers(10,20)
+
+def SubIntegers(a,b):
+    return a-b
+print(SubIntegers(10,20))
+
